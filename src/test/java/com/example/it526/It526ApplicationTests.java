4b113622addfad81fb0e93d8269bd92ef8df189f
Ismail-Taswar <17ps093@bsmrstu.edu.bd>
2025-06-21 19:40:53 +0600

    Initial commit

---
+++ src/test/java/com/example/it526/It526ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it526;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It526ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
